00539acbb9370ec0165be1161bf7ece544ebfcd9
hiranmaya <hiranmayamallik000@gmail.com>
2025-01-03 20:40:57 +0530

    Ternary operator

---
+++ 7. Ternary_operator/script.js
@@ -0,0 +1,8 @@
+const username = 5 > 2 ? 'debu' : 'raja';
+// console.log(username)
+
+let gender = 'f';
+
+const userName = `${gender === 'f' ? 'She' : 'He'} is a college student`;
+
+// console.log(userName)
