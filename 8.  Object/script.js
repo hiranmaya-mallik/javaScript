f91f6f42315f64dfb5e82a202d80ae24fa36b602
hiranmaya <hiranmayamallik000@gmail.com>
2025-01-04 19:48:56 +0530

    8.  Object

---
+++ 8.  Object/script.js
@@ -0,0 +1,27 @@
+// what is object :- we used to stored collection of data. 
+
+
+let user = {
+    name: 'Hiranmaya Mallik', // here name is key and hiranmaya mallik is value
+    age: 20,
+}
+user.adress = 'haldipada'
+user["is-Student"] = true
+
+// console.log(user)
+
+
+let user1 = {
+    firstName: 'Hiranmaya',
+    lastName: 'Mallik',
+    age: 20,
+    adress:{
+        city: 'Bhubaneswar',
+        state: 'Odisha',
+        pinCode: 75756
+    }
+}
+// Object.seal(user1)  // when we use seal function thats lock to the main object and we not changes this property.
+
+Object.freeze(user1) // working same as well as seal function.
+console.log(user1)
