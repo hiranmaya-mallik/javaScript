8acc96149febe6b0faf3dd7d56563e70a81593cf
hiranmaya <hiranmayamallik000@gmail.com>
2025-01-09 20:35:02 +0530

    first commit

---
+++ 11. Loops/While/script.js
@@ -0,0 +1,26 @@
+// let i = 0;
+
+// while(i < 10){
+//     console.log(num + 1)
+//     i++;
+// }
+
+// Create a array and give some value and print all value using while loop.
+
+let friends = ['Hiranmaya', 'Ranjit', 'Rajesh', 'Tanmay', 'Arjit', 'Deba'];
+
+// let j = 0
+// while(j < friends.length){
+//     console.log(friends[j])
+//     j++;
+// }
+
+//Write a program that repeatedly takes input from the user until they enter the word "stop."
+
+let a = prompt('Please enter the word "stop"')
+
+while(a === 'stop'){
+    console.log('Thank You');
+    // a = prompt('Please enter the word "stop"');
+    a++;
+}
